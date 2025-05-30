65bd9726c1b85043623626bc80b336a56b39381b
Mael <mame.duvey@gmail.com>
2025-05-30 09:44:02 +0200

    variance and std dev

---
+++ statistics/variance.py
@@ -0,0 +1,34 @@
+# Variance is a measure of the average difference beetween the average and the values of a set ^ 2
+# We do it sqrt because we want to work with abs values
+# Standard deviation = sqrt of variance
+
+import numpy as np
+import statistics as stats
+
+dataset1 = [1, 6, 42, 2, 8]
+dataset2 = [5, 5, 5, 5, 5]
+
+def var(dataset):
+	average = np.average(dataset)
+	totalSum = 0
+	for i in range(len(dataset)):
+		diff = np.square(dataset[i] - average)
+		totalSum += diff
+	return (totalSum / (len(dataset) - 1))
+
+
+def stdev(dataset):
+	return (np.sqrt(var(dataset)))
+
+print('dataset1 variance: ', var(dataset1))
+print('(with numpy): ', np.var(dataset1, ddof=1)) # use simple instead of population
+print('dataset1 standard deviation: ', stdev(dataset1))
+print('(with numpy): ', stats.stdev(dataset1))
+
+print('dataset2 variance: ', var(dataset2))
+print('(with numpy): ', np.var(dataset2, ddof=1)) # use simple instead of population
+print('dataset2 standard deviation: ', stdev(dataset2))
+print('(with numpy): ', stats.stdev(dataset2))
+
+
+
